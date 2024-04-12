ef9a45a036c1d779852cf0af03fe41fe4cbe1d9f
CadmoNeto <cadmonneto@gmail.com>
2024-04-12 10:59:39 -0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-    return <p>Dashboard Page</p>
+import { Card } from "../ui/dashboard/cards";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { lusitana } from "../ui/fonts";
+import { fetchRevenue } from "../lib/data";
+
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    return(
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-x1 md:text-2x1`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 ld:grid-cols-4">
+                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+                {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+                {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+                {/* <Card title="Total Costumers" value={numberOfCustomers} type="customers" /> */}
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <RevenueChart revenue={revenue} />
+                {/* <LatestInvoices latestInvoices={LatestInvoices} /> */}
+            </div>
+        </main>
+    );
 }
